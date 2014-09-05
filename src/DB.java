00e90331c701a3ae47643dc04ba89f3c7a0c40a5
sridharrajs <sridhar.m2h@gmail.com>
2014-09-05 15:07:56 +0530

    moving files to root level

---
+++ src/DB.java
@@ -0,0 +1,40 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+ 
+public class DB {
+ 
+	public Connection conn = null;
+ 
+	public DB() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://localhost:3306/Crawler";
+			conn = DriverManager.getConnection(url, "root", "root");
+			System.out.println("conn built");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+ 
+	public ResultSet runSql(String sql) throws SQLException {
+		Statement sta = conn.createStatement();
+		return sta.executeQuery(sql);
+	}
+ 
+	public boolean runSql2(String sql) throws SQLException {
+		Statement sta = conn.createStatement();
+		return sta.execute(sql);
+	}
+ 
+	@Override
+	protected void finalize() throws Throwable {
+		if (conn != null || !conn.isClosed()) {
+			conn.close();
+		}
+	}
+}
